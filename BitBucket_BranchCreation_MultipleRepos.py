a668904b2dffdfd3f6777cda9c75cdb096f24437
gcotto92 <gcotto92@gmail.com>
2023-01-22 18:13:40 -0800

    Update BitBucket_BranchCreation_MultipleRepos.py

--- BitBucket_BranchCreation_MultipleRepos.py
+++ BitBucket_BranchCreation_MultipleRepos.py
@@ -2,16 +2,16 @@ from atlassian import Bitbucket
 
 #Enter the URL and the Bitbucket Personal Access Token
 bitbucket = Bitbucket(
-    url = 'https://git.rostermonster.com/',
+    url = '',
     token= ''
 )
 
-#RAC is the project Name for RosterApps-Config
-project = "RAC"
-#all is the repo of /RAC/all
-repository = "all"
+
+project = ""
+
+repository = ""
 #name variable is for the name of the new branch to be created
-name = 'release/23.08'
+name = 'release'
 #start_point variable is used as the origin branch to create the new one from
 start_point = 'release/23.06'
 message = 'Created via python automation script'
@@ -20,9 +20,9 @@ message = 'Created via python automation script'
 bitbucket.create_branch(project, repository, name, start_point, message)
 print(f'Created branch of {name} in {repository}')
 
-#ANS is the project name for Ansible
-project2 = "ANS"
-#repository2 is the repo of Ansible/rosterapps
-repository2 = "rosterapps"
+
+project2 = ""
+
+repository2 = "release"
 bitbucket.create_branch(project2, repository2, name, start_point, message)
 print(f'Created branch of {name} in {repository2}')
