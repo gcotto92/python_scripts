bb0655f2a9e86a18904bd6d6431a996f382a9cf4
gio cotto <gcotto92@gmail.com>
2023-01-23 09:31:46 -0800

    Recreated 3 scripts with template info

--- BitBucket_BranchCreation_MultipleRepo.py
+++ BitBucket_BranchCreation_MultipleRepo.py
@@ -2,27 +2,27 @@ from atlassian import Bitbucket
 
 #Enter the URL and the Bitbucket Personal Access Token
 bitbucket = Bitbucket(
-    url = 'https://git.rostermonster.com/',
+    url = '',
     token= ''
 )
 
-#RAC is the project Name for RosterApps-Config
-project = "RAC"
+
+project = ""
 #all is the repo of /RAC/all
-repository = "all"
+repository = ""
 #name variable is for the name of the new branch to be created
-name = 'release/23.08'
+name = 'new_branch'
 #start_point variable is used as the origin branch to create the new one from
-start_point = 'release/23.06'
+start_point = 'origin_branch'
 message = 'Created via python automation script'
 
 #bitbucket.create_branch(project, repository, name)
 bitbucket.create_branch(project, repository, name, start_point, message)
 print(f'Created branch of {name} in {repository}')
 
-#ANS is the project name for Ansible
-project2 = "ANS"
+
+project2 = ""
 #repository2 is the repo of Ansible/rosterapps
-repository2 = "rosterapps"
+repository2 = ""
 bitbucket.create_branch(project2, repository2, name, start_point, message)
 print(f'Created branch of {name} in {repository2}')
