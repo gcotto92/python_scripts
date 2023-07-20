051a25fac1de6ff7d8f92b72eafdc17113b4e3e1
gcotto92 <gcotto92@gmail.com>
2023-07-20 17:40:06 -0400

    Added v2 of Download_Torrent.py

--- Download_Torrentv2.py
+++ Download_Torrentv2.py
@@ -2,7 +2,7 @@ from qbittorrent import Client
 
 qb = Client('http://127.0.0.1:8080/')
 
-qb.login('admin','batsu1992')
+qb.login('admin','passwordgoeshere')
 
 torrent_path = input('Enter the torrent path:\n')
 torrent_file = open(f'{torrent_path}', 'rb')
